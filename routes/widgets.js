12d3c29b90aa0f2349feb5bb6608288893c6960e
Amal Botan <amalbotan254@gmail.com>
2022-02-12 20:04:35 +0000

    cleanup

--- routes/widgets.js
+++ routes/widgets.js
@@ -5,14 +5,13 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-// hello world
-//helloooooooo
+
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets, sjnjdsj`;
+    let query = `SELECT * FROM widgets`;
     console.log(query);
     db.query(query)
       .then(data => {
