11a1de5f7d56fd0cf72592f40fe7e7d9a145387d
Amal <amalbotan254@gmail.com>
2022-02-13 00:43:20 -0500

    User's icon highlights when their audio is being picked up

--- client/src/components/Tile/User.jsx
+++ client/src/components/Tile/User.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import UserTile from './UserTile';
 import UserWrapper from './UserWrapper';
 import UserInfo from './UserInfo';
+import {
+  useHMSStore,
+  selectPeerAudioByID,
+  selectIsPeerAudioEnabled,
+} from '@100mslive/hms-video-react';
+
+
 
 const User = ({ peer }) => {
-  const audioEnabled = false;
-  const level = 0;
+  const level = useHMSStore(selectPeerAudioByID(peer.id)) || 0;
+  const audioEnabled = useHMSStore(selectIsPeerAudioEnabled(peer.id));
   return (
     <UserTile>
       <UserWrapper level={level}>
