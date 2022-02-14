cba4ca20d19e5bea218b9056d3d0034dc0808417
hsaidahmed <hashim.saidahmed@gmail.com>
2022-02-14 01:04:28 -0500

    adding images to users table

--- server/db/seeds/02_schema.sql
+++ server/db/seeds/02_schema.sql
@@ -1,7 +1,7 @@
-INSERT INTO users (first_name, last_name, username, email, password, bio)
-VALUES ('Steph', 'Bob', 'steph.bob', 'stephb@gmail.com', 'password', 'bio description'),
-('Steve', 'Sam', 'steve.sam', 'steves@gmail.com', 'password', 'bio description'),
-('Emily', 'Jack', 'emily.jack', 'emilyj@gmail.com', 'password', 'bio description');
+INSERT INTO users (first_name, last_name, username, email, user_image, password, bio)
+VALUES ('Steph', 'Bob', 'steph.bob', 'stephb@gmail.com', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfSN0LzT35OFBzyTtlHIl876rIsAcWWen9nA&usqp=CAU', 'password', 'bio description'),
+('Steve', 'Sam', 'steve.sam', 'steves@gmail.com', 'https://mpng.subpng.com/20180308/jdw/kisspng-avatar-scalable-vector-graphics-user-profile-icon-wearing-a-striped-shirt-boys-5aa11b77789686.8517616415205077674939.jpg', 'password', 'bio description'),
+('Emily', 'Jack', 'emily.jack', 'emilyj@gmail.com', 'https://cdn.imgbin.com/10/20/11/imgbin-avatar-user-profile-icon-women-wear-frock-GNLBV4N6qanFpjEYMEqxEgcCd.jpg', 'password', 'bio description');
 
 
 INSERT INTO posts (user_id, post_text, likes)
@@ -11,12 +11,11 @@ VALUES (1, 'Hello everyone!', 5),
 (2, 'Its so cold outside, what should I put on my baby?', 7),
 (3, 'My kids wont listen when I put them to bed! Any tips?', 10);
 
-INSERT INTO categories (post_id,category)
+INSERT INTO categories (post_id, category)
 VALUES (2, 'showering'), (3, 'feeding'), (4, 'temp');
 
 INSERT INTO comments (user_id, post_id, comment, likes)
 VALUES (1, 1, 'Hey!!', 2),
 (2, 2, 'Get a baby shower top, make sure the water is warm, get a washcloth and some soap and scrub away (gently of course)!!', 10),
 (3, 2, 'Shower before bed', 10),
-(2,3, 'Its okay, babies get like that sometimes! Offer the breastmilk, and you can top up with formula if you would like! Also talk to your doctor to see if nothing else is wrong', 12);
-
+(2, 3, 'Its okay, babies get like that sometimes! Offer the breastmilk, and you can top up with formula if you would like! Also talk to your doctor to see if nothing else is wrong', 12);
