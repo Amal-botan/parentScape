dee5bdc1caaaa0f6cdf50c3225db452649f86987
Nadia <nadi.fatah@gmail.com>
2022-02-15 05:00:18 -0500

    Add PostForm to ParentFeed route

--- client/src/screens/ParentFeedScreen.jsx
+++ client/src/screens/ParentFeedScreen.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Post from "../components/Post";
+import PostForm from "../components/PostForm"
 import "../components/Post.css";
+import "../components/PostForm.css";
 
 const ParentFeedScreen = () => {
   return (
 <>
+<PostForm />
 <Post />
 
 
