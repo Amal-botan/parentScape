1019eb3d8b3a8bcc441ccdf264fd76e0136a49f7
Nadia <nadi.fatah@gmail.com>
2022-02-15 05:58:44 -0500

    Styling

--- client/src/components/PostForm.jsx
+++ client/src/components/PostForm.jsx
@@ -5,9 +5,11 @@ export default function PostForm() {
   return (
     <main className="container">
         <textarea className="form__textarea" name="text" placeholder={posts}></textarea>
+        <div id="buttons">
         <input type="submit" value="Post" className="form__input" />
         <input type="submit" value="Add Category" className="form__input" />
         <input type="submit" value="Add Image" className="form__input" />
+        </div>
     </main>
   );
 }
