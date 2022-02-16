d54d695fd25cb53a2605cf259715c7ba241f8185
hsaidahmed <hashim.saidahmed@gmail.com>
2022-02-16 01:40:54 -0500

    Profile page

---
+++ client/src/components/ProfileChildren.jsx
@@ -0,0 +1,17 @@
+import "./ProfileChildren.css"
+function ProfileChildren() {
+
+  return (
+<div class="title">
+  <span>My children</span>
+  <div class="names">
+  <p>Child 1</p>
+  <p>Child 1</p>
+  <p>Child 1</p>
+  <p>Child 1</p>
+  
+  </div>
+</div>
+  )
+}
+export default ProfileChildren;
