7224d184c76527fa6fdc77eb0aef1a9f6a0c82c6
hsaidahmed <hashim.saidahmed@gmail.com>
2022-02-16 04:29:09 -0500

    design children block

--- client/src/components/ProfileChildren.jsx
+++ client/src/components/ProfileChildren.jsx
@@ -1,16 +1,40 @@
 import "./ProfileChildren.css"
+
+
+
+
+
 function ProfileChildren() {
 
   return (
+<div class="main">
 <div class="title">
-  <span>My children</span>
-  <div class="names">
-  <p>Child 1</p>
-  <p>Child 1</p>
-  <p>Child 1</p>
-  <p>Child 1</p>
-  
+  <h1>My Children</h1>
   </div>
+  
+  
+  <div class="names">
+  <h2>Child 1</h2>
+  <h2>1 year old</h2>
+  </div>
+  <div class="names">
+  <h2>Child 1</h2>
+  <h2>2 years old</h2>
+
+  </div>
+  
+  <div class="names">
+  <h2>Child 1</h2>
+  <h2>3 years old</h2>
+
+  </div>
+  <div class="names">
+  <h2>Child 1</h2>
+  <h2>4 years old</h2>
+
+  </div>
+  
+
 </div>
   )
 }
