7e004514708e0b0a5f1a7d78109103a5b941dbba
Nadia <nadi.fatah@gmail.com>
2022-02-15 20:29:35 -0500

    Parent feed styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -160,9 +160,7 @@ const Navbars = () => {
       </nav>
 
      
-        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">Welcome to ParentScape</h1>
-        </div>
+       
       
     </div>
   );
