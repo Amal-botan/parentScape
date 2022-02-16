12e255dab53700206db6ff34e59a7ce4c196db87
hsaidahmed <hashim.saidahmed@gmail.com>
2022-02-16 04:36:58 -0500

    Implement profile screen routes

--- client/src/components/ProfileChildren.jsx
+++ client/src/components/ProfileChildren.jsx
@@ -1,41 +1,30 @@
-import "./ProfileChildren.css"
-
-
-
-
+import "./ProfileChildren.css";
 
 function ProfileChildren() {
-
   return (
-<div class="main">
-<div class="title">
-  <h1>My Children</h1>
-  </div>
-  
-  
-  <div class="names">
-  <h2>Child 1</h2>
-  <h2>1 year old</h2>
-  </div>
-  <div class="names">
-  <h2>Child 1</h2>
-  <h2>2 years old</h2>
+    <div class="main">
+      <div class="title">
+        <h1>My Children</h1>
+      </div>
 
-  </div>
-  
-  <div class="names">
-  <h2>Child 1</h2>
-  <h2>3 years old</h2>
+      <div class="names">
+        <h2>Child 1</h2>
+        <h2>1 year old</h2>
+      </div>
+      <div class="names">
+        <h2>Child 1</h2>
+        <h2>2 years old</h2>
+      </div>
 
-  </div>
-  <div class="names">
-  <h2>Child 1</h2>
-  <h2>4 years old</h2>
-
-  </div>
-  
-
-</div>
-  )
+      <div class="names">
+        <h2>Child 1</h2>
+        <h2>3 years old</h2>
+      </div>
+      <div class="names">
+        <h2>Child 1</h2>
+        <h2>4 years old</h2>
+      </div>
+    </div>
+  );
 }
 export default ProfileChildren;
