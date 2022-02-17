d326adf34fb6e7f75f19472f9627836a2c56206c
Nadia <nadi.fatah@gmail.com>
2022-02-17 11:22:16 -0500

    Add clickable markers, toronto markers and center and resize map size

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,24 +1,69 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const mapStyles = {        
+  height: "50vh",
+  width: "50%"};
+
+  const defaultCenter = {
+    lat: 43.785663062587105, lng: -79.43435624397236
+  }
 
 const Map = () => {
-  
-  const mapStyles = {        
-    height: "100vh",
-    width: "100%"};
-  
-  const defaultCenter = {
-    lat: 43.71524289323266, long: -79.35833041608609
-  }
-  
+
+  const locations = [
+    {
+      name: "Location 1",
+      location: { 
+        lat: 43.77510632345708, 
+        lng: -79.44736634716638
+      },
+    },
+    {
+      name: "Location 2",
+      location: { 
+        lat: 43.76642919663482, 
+        lng: -79.42127381772258
+      },
+    },
+    {
+      name: "Location 3",
+      location: { 
+        lat: 43.789135045529704, 
+        lng: -79.41835079686906
+      },
+    },
+    {
+      name: "Location 4",
+      location: { 
+        lat: 43.77994017660731, 
+        lng: -79.41578065362916
+      },
+    },
+    {
+      name: "Location 5",
+      location: { 
+        lat: 43.80397969069323,
+        lng: -79.44805299267806
+      },
+    }
+  ];
+ 
   return (
      <LoadScript
        googleMapsApiKey='AIzaSyDutQF2kdYj4SC4efZscY_CamfDi24axB8'>
-        <GoogleMap
+       <GoogleMap
           mapContainerStyle={mapStyles}
           zoom={13}
-          center={defaultCenter}
-        />
+          center={defaultCenter}>
+         {
+            locations.map(item => {
+              return (
+              <Marker key={item.name} position={item.location}/>
+              )
+            })
+         }
+     </GoogleMap>
      </LoadScript>
   )
 }
