4e611f3ac97768815e91432cbb48968f832de879
hsaidahmed <hashim.saidahmed@gmail.com>
2022-02-17 23:52:30 -0500

    Redesign post form

--- client/src/components/PostForm.jsx
+++ client/src/components/PostForm.jsx
@@ -1,58 +1,129 @@
 import React from "react";
-import "./PostForm.css"
+import "./PostForm.css";
 import { useState, useEffect } from "react";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import Stack from "@mui/material/Stack";
+import SendIcon from "@mui/icons-material/Send";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function PostForm(props) {
-  const [postText, setPostText] = useState("")
-  const [categoryText, setCategory] = useState("")
-  const [addcategory, setAddCategory] = useState(false)
-  const [addImage, setAddImage] = useState(false)
-  const [image, setImage] = useState(false)
+  const [postText, setPostText] = useState("");
+  const [categoryText, setCategory] = useState("");
+  const [addcategory, setAddCategory] = useState(false);
+  const [addImage, setAddImage] = useState(false);
+  const [image, setImage] = useState(false);
 
   const handleSubmit = () => {
-    const post = { post_text: postText, category: categoryText }
-    console.log(post)
-    props.addPost(post)
-  }
+    const post = { post_text: postText, category: categoryText };
+    console.log(post);
+    props.addPost(post);
+  };
 
   const handleCategoryButton = () => {
     if (addcategory === false) {
-      setAddCategory(true)
+      setAddCategory(true);
     } else {
-      setAddCategory(false)
+      setAddCategory(false);
     }
-  }
+  };
 
   const handleImageButton = () => {
     if (addImage === false) {
-      setAddImage(true)
+      setAddImage(true);
     } else {
-      setAddImage(false)
+      setAddImage(false);
     }
-  }
-  //Add image part 
+  };
+  const Input = styled("input")({
+    display: "none",
+  });
+  //Add image part
   return (
-    <main className="container">
-      <textarea className="form__textarea" name="text" placeholder="What is on your mind?" value={postText} onChange={(event) => setPostText(event.target.value)} ></textarea>
+    <div className="tweet-wrap">
+      <textarea
+        className="form__textarea"
+        name="text"
+        placeholder="What is on your mind?"
+        value={postText}
+        onChange={(event) => setPostText(event.target.value)}
+      ></textarea>
       <div id="buttons">
-        {!addcategory && <button class="buttons" type="submit" value="Add Category" onClick={handleCategoryButton}>Add Category</button>}
+        {!addcategory && (
+          <Button
+            variant="contained"
+            component="span"
+            onClick={handleCategoryButton}
+          >
+            Add Category
+          </Button>
+        )}
+
+        {addcategory && (
+          <button
+            type="submit"
+            value="Add Category"
+            onClick={handleCategoryButton}
+          >
+            X
+          </button>
+        )}
+
+        {/* {!addImage && <button class="buttons" type="submit" value="Add Category" onClick={handleImageButton}>Add Image</button>} */}
+
+        {addImage && (
+          <button
+            type="submit"
+            value="Add Category"
+            onClick={handleImageButton}
+          >
+            X
+          </button>
+        )}
+
+        {addcategory && (
+          <textarea
+            className="form__textarea"
+            name="text"
+            placeholder="Add category"
+            value={categoryText}
+            onChange={(event) => setCategory(event.target.value)}
+          ></textarea>
+        )}
+        {addImage && (
+          <textarea
+            className="form__textarea"
+            name="text"
+            placeholder="Add category"
+            value={categoryText}
+            onChange={(event) => setImage(event.target.value)}
+          ></textarea>
+        )}
+        <label htmlFor="contained-button-file">
+          <Input
+            accept="image/*"
+            id="contained-button-file"
+            multiple
+            type="file"
+          />
+          <Button variant="contained" component="span">
+            Upload
+          </Button>
+        </label>
+        <Button
+          variant="contained"
+          size="small"
+          endIcon={<SendIcon />}
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Post
+        </Button>
         
-        {addcategory && <button type="submit" value="Add Category" onClick={handleCategoryButton}>X</button>}
-
-        {!addImage && <button class="buttons" type="submit" value="Add Category" onClick={handleImageButton}>Add Image</button>}
-
-        {addImage && <button type="submit" value="Add Category" onClick={handleImageButton}>X</button>}
-
-        {addcategory &&
-
-          <textarea className="form__textarea" name="text" placeholder="Add category" value={categoryText} onChange={(event) => setCategory(event.target.value)} ></textarea>}
-
-        {addImage &&
-
-          <textarea className="form__textarea" name="text" placeholder="Add category" value={categoryText} onChange={(event) => setImage(event.target.value)} ></textarea>}
-
-        <input type="submit" value="Post" className="form__input" onClick={handleSubmit} />
+        {/* <input type="submit" value="Post" className="form__input" onClick={handleSubmit} /> */}
       </div>
-    </main>
+    </div>
   );
 }
